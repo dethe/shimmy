6e55894378d3e591e321d715ca3ab7d95df78c4f
Glitch (drawingboard) <none>
2019-09-01 20:49:25 +0000

    🐢📇 Checkpoint ./js/stepper.js:739936/507

--- js/stepper.js
+++ js/stepper.js
@@ -1,4 +1,5 @@
 // Intended to enhance UI without being required. Will find any <input type="number" and replace them with this control to provide better visibility and larger targets for the stepper.
+// Based on the Stepper component in Form Design Patterns, by Adam Silver (starts on p. 181)
 
 // CSS 
 
@@ -9,11 +10,25 @@ input::-webkit-inner-spin-button{
   appearance: none;
   margin: 0;
 }
+input[type="number"] {
+    -moz-appearance: textfield;
+}
 `;
-document.head.append(dom.element('style', {}, css_rules));
+
+function addCSS(){
+  let style = document.createElement('style');
+  style.innerText = css_rules;
+  document.head.append(style);
+}
+addCSS();
+
+document.head.append(
+  dom.element('style', {}, css_rules));
 
 function upgrade(input){
   
+  input.outerHTML = `<div class="field">
+  `;
 }
 
-document.querySelectorAll('input ')
+Array.from(document.querySelectorAll('input[type=number]')).forEach(upgrade);
