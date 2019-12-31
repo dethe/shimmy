88d5ae4bf80f38dad70e1692d6d64d3ce9da6eee
Glitch (drawingboard) <none>
2019-12-31 21:37:16 +0000

    🏮🏞 Checkpoint ./js/script.js:739936/245

--- js/script.js
+++ js/script.js
@@ -1072,7 +1072,10 @@ document.addEventListener("keydown", hotkeys, false);
 // disable default Safari iOS double-tap to zoom and pinch to zoom
 
 document.addEventListener('touchmove', function (event) {
-  if (event.scale !== 1) { event.preventDefault(); }
+  if (event.scale !== 1) { event.preventDefault();  event.stopPropagation();}
+  if (event.touches.length > 1){
+    event.preventDefault(); event.stopPropagation();
+  }
 }, false);
 
 var lastTouchEnd = 0;
