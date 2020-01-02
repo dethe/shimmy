aefe54caeb1f405b206faec0fea22d3002908116
Glitch (drawingboard) <none>
2020-01-02 22:08:14 +0000

    📚😇 Checkpoint ./js/undo.js:739936/1487

--- js/undo.js
+++ js/undo.js
@@ -9,4 +9,43 @@
 // undo(type); pops the relevant undo stack
 // redo(type); pops the relevant redo stack
 // switchFrame(newFrame); // because most undo is frame-based
-// clear()
+// clear(); // reset, i.e., when a new document is created
+//
+// Use events for enabling/disabling buttons and changing button labels
+// Events: document.addEventListener('shimmy-undo-change', handler, true);
+// function undoHandler(evt){
+//   evt.frameUndo = nameOfFrameUndo or null;
+//   evt.frameRedo = nameOfFrameRedo or null;
+//   evt.docUndo = nameOfDocUndo or null;
+//   evt.docRedo = nameOfDocRedo or null;
+// }
+
+(function(){
+  const documentUndoStack = [];
+  const documentRedoStack = [];
+  const framesUndoStack = new Map();
+  const frameRedoStack = new Map();
+  let currentFrame = null;
+  
+  const init = (frame) => 
+  
+  const pushUndo = (name, type, applyFn, restoreFn) => {
+    
+  };
+  
+  const undo = (type) => {
+    
+  }
+  
+  const redo = (type) => {
+    
+  }
+  
+  window.undoredo = {
+    init(currentFrame);
+    undo,
+    redo,
+    pushUndo
+  }
+  
+})();
