71aff11ba8f28f0523e6a131d29caa2a214839b6
Dethe Elza <dethe@livingcode.org>
2022-06-08 16:36:53 -0700

    removed duplicate from cache list

--- js/sw.js
+++ js/sw.js
@@ -1,25 +1,31 @@
 // Service worker for Shimmy to work offline
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log("[Service Worker] Caching all: app shell and content");
-    await cache.addAll(appShellFiles);
-  })());
+self.addEventListener("install", e => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(appShellFiles);
+    })()
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) { return r; }
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
 });
 
 const cacheName = "shimmy-v1";
@@ -80,6 +86,5 @@ const appShellFiles = [
   "../logo/logo0_256.png",
   "../logo/logo0_384.png",
   "../logo/logo0_512.png",
-  "../logo/logo0_60.png",
-  "../favicon.ico"
+  "../favicon.ico",
 ];
